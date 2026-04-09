a6a2936ac70f4fd187784a941a516f27a880c97c
Asefa Tolessa Dano <asetolessa711@gmail.com>
2026-04-09 15:12:53 +0000

    feat: add order and invoice external-id foundation

--- backend/utils/externalId.js
+++ backend/utils/externalId.js
@@ -2,10 +2,18 @@ const crypto = require('crypto');
 
 const EXTERNAL_ID_PREFIX = 'uid';
 const PRODUCT_EXTERNAL_ID_PREFIX = 'pid';
+const ORDER_EXTERNAL_ID_PREFIX = 'oid';
+const INVOICE_EXTERNAL_ID_PREFIX = 'iid';
 const EXTERNAL_ID_BODY_LENGTH = 20;
 const EXTERNAL_ID_PATTERN = new RegExp(`^${EXTERNAL_ID_PREFIX}_[a-f0-9]{${EXTERNAL_ID_BODY_LENGTH}}$`);
 const PRODUCT_EXTERNAL_ID_PATTERN = new RegExp(
   `^${PRODUCT_EXTERNAL_ID_PREFIX}_[a-f0-9]{${EXTERNAL_ID_BODY_LENGTH}}$`
+);
+const ORDER_EXTERNAL_ID_PATTERN = new RegExp(
+  `^${ORDER_EXTERNAL_ID_PREFIX}_[a-f0-9]{${EXTERNAL_ID_BODY_LENGTH}}$`
+);
+const INVOICE_EXTERNAL_ID_PATTERN = new RegExp(
+  `^${INVOICE_EXTERNAL_ID_PREFIX}_[a-f0-9]{${EXTERNAL_ID_BODY_LENGTH}}$`
 );
 const LEGACY_MONGO_ID_PATTERN = /^[a-f0-9]{24}$/;
 
@@ -15,6 +23,14 @@ function generateExternalId() {
 
 function generateProductExternalId() {
   return `${PRODUCT_EXTERNAL_ID_PREFIX}_${crypto.randomBytes(10).toString('hex')}`;
+}
+
+function generateOrderExternalId() {
+  return `${ORDER_EXTERNAL_ID_PREFIX}_${crypto.randomBytes(10).toString('hex')}`;
+}
+
+function generateInvoiceExternalId() {
+  return `${INVOICE_EXTERNAL_ID_PREFIX}_${crypto.randomBytes(10).toString('hex')}`;
 }
 
 function generateVendorExternalId() {
@@ -31,6 +47,16 @@ function isValidProductExternalId(value) {
   return PRODUCT_EXTERNAL_ID_PATTERN.test(String(value).trim().toLowerCase());
 }
 
+function isValidOrderExternalId(value) {
+  if (!value) return false;
+  return ORDER_EXTERNAL_ID_PATTERN.test(String(value).trim().toLowerCase());
+}
+
+function isValidInvoiceExternalId(value) {
+  if (!value) return false;
+  return INVOICE_EXTERNAL_ID_PATTERN.test(String(value).trim().toLowerCase());
+}
+
 function isValidVendorExternalId(value) {
   return isValidExternalId(value);
 }
@@ -43,9 +69,13 @@ function isLikelyMongoObjectId(value) {
 module.exports = {
   generateExternalId,
   generateProductExternalId,
+  generateOrderExternalId,
+  generateInvoiceExternalId,
   generateVendorExternalId,
   isLikelyMongoObjectId,
   isValidExternalId,
   isValidProductExternalId,
+  isValidOrderExternalId,
+  isValidInvoiceExternalId,
   isValidVendorExternalId,
 };
